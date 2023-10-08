f4e2e673ffee42a0e32675c5130a2d91e18e1583
Matheus Ryan <mrysn@ic.ufal.br>
2023-10-07 21:45:16 -0300

    Update prim.cpp

--- prim/prim.cpp
+++ prim/prim.cpp
@@ -6,66 +6,64 @@ bool compare(const pair<int, int> &a, const pair<int, int> &b){ //funcao que com
     else  return a.second < b.second;
 }
 
-pair<vector<pair<int, int>>, int> prim(vector<pair<int, int>> adj[], int N, int startVertex)
+pair<vector<pair<int, int>>, int> prim(vector<pair<int, int>> adj[], int N, int init) // Função da árvore geradora mínima 
 {
-    vector<pair<int, int>> result; // vetor de pai (vertice, pai))
+    vector<pair<int, int>> result; // vetor de resultado (vertice, pai))
 
-    int parent[N + 1];
-    int key[N + 1];
-    bool visited[N + 1];
+    int parent[N + 1]; //vetor de pai
+    int cost[N + 1]; //vetor de custo
+    bool visited[N + 1];// vetor de visitado
 
-    for (int i = 1; i <= N; i++){
-        key[i] = INT_MAX;
+    for (int i = 1; i <= N; i++){//Custo infinito e nenhum visitado
+        cost[i] = INT_MAX;
         visited[i] = false;
     }
 
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> queue;//Fila de prioridade
 
-    key[1] = 0;
-    parent[1] = -1;
-    pq.push({0, 1});
+    cost[1] = 0;//custo do vertice 1 = 0
+    parent[1] = -1;//Vertice 1 nao tem pai
+    queue.push({0, 1});
 
-    while (not pq.empty())
+    while (not queue.empty())
     {
-        int u = pq.top().second;
-        pq.pop();
+        int u = queue.top().second;
+        queue.pop();
 
         visited[u] = true;
 
         for (auto sample : adj[u])
         {
             int v = sample.first;
-            int weight = sample.second;
-            if (visited[v] == false && weight < key[v])
+            int force = sample.second;
+            if (visited[v] == false && force < cost[v])
             {
                 parent[v] = u;
-                key[v] = weight;
-                pq.push({key[v], v});
+                cost[v] = force;
+                queue.push({cost[v], v});
             }
         }
     }
 
-    int totalCost = 0;
+    int summcost = 0;
     for (int i = 2; i <= N; i++)
     {
         result.push_back(make_pair(i, parent[i]));
-        totalCost += key[i];
+        summcost += cost[i];
     }
 
-    return make_pair(result, totalCost);
+    return make_pair(result, summcost);
 }
 
 int main(int argc, char *argv[])
 {
-    string input_file = "";
-    string output_file = "";
+    string in = "";
+    string out = "";
     bool ans = false;
-    int startVertex = 1;
+    int init = 1;
 
-    for (int i = 1; i < argc; i++)
-    {
-        if (strcmp(argv[i], "-h") == 0)
-        {
+    for (int i = 1; i < argc; i++){
+        if (strcmp(argv[i], "-h") == 0){
             cout << "Help:" << endl;
             cout << "-h: mostra o help" << endl;
             cout << "-o <arquivo>: redireciona a saida para o 'arquivo'" << endl;
@@ -74,34 +72,28 @@ int main(int argc, char *argv[])
             cout << "-i: vértice inicial (para o algoritmo de Prim)" << endl;
             return 0;
         }
-        else if (strcmp(argv[i], "-o") == 0 && i < argc - 1)
-        {
-            output_file = argv[++i];
+        else if (strcmp(argv[i], "-o") == 0 && i < argc - 1){
+            out = argv[++i];
         }
-        else if (strcmp(argv[i], "-f") == 0 && i < argc - 1)
-        {
-            input_file = argv[++i];
+        else if (strcmp(argv[i], "-f") == 0 && i < argc - 1){
+            in = argv[++i];
         }
-        else if (strcmp(argv[i], "-s") == 0)
-        {
+        else if (strcmp(argv[i], "-s") == 0){
             ans = true;
         }
-        else if (strcmp(argv[i], "-i") == 0 && i < argc - 1)
-        {
-            startVertex = atoi(argv[++i]);
+        else if (strcmp(argv[i], "-i") == 0 && i < argc - 1){
+            init = atoi(argv[++i]);
         }
     }
 
-    if (input_file == "")
-    {
+    if (in == ""){
         cerr << "No input file specified. Use the -f parameter." << endl;
         return 1;
     }
 
-    ifstream fin(input_file);
-    if (!fin)
-    {
-        cerr << "Could not open input file: " << input_file << endl;
+    ifstream fin(in);
+    if (!fin){
+        cerr << "Could not open input file: " << in << endl;
         return 1;
     }
 
@@ -110,8 +102,7 @@ int main(int argc, char *argv[])
     vector<pair<int, int>> adj[N + 1];
 
     int a, b, wt;
-    for (int i = 0; i < m; i++)
-    {
+    for (int i = 0; i < m; i++){
         fin >> a >> b >> wt;
         adj[a].push_back(make_pair(b, wt));
         adj[b].push_back(make_pair(a, wt));
@@ -119,40 +110,32 @@ int main(int argc, char *argv[])
 
     fin.close();
 
-    pair<vector<pair<int, int>>, int> result = prim(adj, N, startVertex);
+    pair<vector<pair<int, int>>, int> result = prim(adj, N, init);
 
-    vector<pair<int, int>> pairsArray = result.first; // Extrair os pares do vetor result
+    vector<pair<int, int>> pairsArray = result.first; 
+    sort(pairsArray.begin(), pairsArray.end(), compare);
 
-    sort(pairsArray.begin(), pairsArray.end(), compare); // Ordenar o vetor de pares
-
-    if (!(output_file == ""))
-    {
-        ofstream fout(output_file);
-        if (!fout)
-        {
-            cerr << "Could not open output file: " << output_file << endl;
+    if (!(out == "")){
+        ofstream fout(out);
+        if (!fout){
+            cerr << "Could not open output file: " << out << endl;
             return 1;
         }
-        if (ans)
-        {
+        if (ans){
             for (auto sample : pairsArray)
                 fout << "(" << sample.second << "," << sample.first << ") ";
         }
-        else
-        {
+        else{
             fout << " " << result.second << endl;
         }
-
         fout.close();
     }
 
-    if (ans)
-    {
+    if(ans){
         for (auto sample : pairsArray)
             cout << "(" << sample.second << "," << sample.first << ") ";
     }
-    else
-    {
+    else{
         cout << " " << result.second << endl;
     }
 
